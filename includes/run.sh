f6bca18f1d9fd08cd23c2485980d87363baa0d34
Leslie-Alexandre DENIS <leslie-alexandre.denis@cc.in2p3.fr>
2015-08-10 14:44:44 +0200

    Rserve startup command modified

--- includes/run.sh
+++ includes/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service postgresql start
-sudo -u tomcat7 /opt/R/bin/R CMD Rserve --slave --vanilla --RS-conf /etc/Rserve.conf
+sudo -u tomcat7 R CMD Rserve --slave --no-save --no-restore --no-init-file --RS-conf /etc/Rserve.conf
 screen -AdmS SolR make -C /tmp/transmart-data/solr/ start
 sudo -u tomcat7 /usr/share/tomcat7/bin/catalina.sh run
 
